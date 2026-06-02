e1b823cb37babc3d8231d2f3603cf301d868d458
naveedakhtar41376 <naveedakhtar4137@gmail.com>
2026-06-02 12:04:02 +0200

    Update hash_fucntions.py
    describing the module

--- hash_fucntions.py
+++ hash_fucntions.py
@@ -1,1 +1,8 @@
+"""
+
+The hash fucntions for bloom filter project
+In this module, using double hashing, we define a family of hash functions. 
+Objective: For each input item, we want to generate k different hash positions
+
+"""
 
